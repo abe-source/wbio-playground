c8d4d81dedc7dd1229213c41be82cf6195b283b7
Arminas B <arminasb7@gmail.com>
2025-07-07 07:30:57 +0300

    add appium packages and configuration

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -102,7 +102,15 @@ export const config: WebdriverIO.Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: [
+        ['appium', {
+            command: 'appium',
+            args: {
+                basePath: '/wd/hub',
+                port: 4723
+            }
+        }]
+    ],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
